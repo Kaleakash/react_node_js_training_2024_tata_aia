0df9e75715477a458bcf68280524ce2bec3f7ad8
akash <akash300383@gmail.com>
2024-06-09 10:42:43 +0530

    done

---
+++ React JS Programs - Intermediate/React JS Programs/react-event-app/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+import EventExample from './EventExample';
+
+function App() {
+  return (
+    <div className="App">
+<h2>Day 2 React JS Programs - Event Handling </h2>
+      <EventExample></EventExample>
+    </div>
+  );
+}
+
+export default App;
---
+++ React JS Programs - Intermediate/React JS Programs/react-event-app/src/EventExample.js
@@ -0,0 +1,11 @@
+function EventExample() {
+
+
+    return(
+        <div>
+            <h3>Event Example</h3>
+        </div>
+    )
+}
+
+export default EventExample;
