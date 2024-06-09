cc7ba4bd355ae9900b9b623ec289ceb4554c8ff0
akash <akash300383@gmail.com>
2024-06-09 10:58:07 +0530

    done

--- React JS Programs - Intermediate/React JS Programs/react-event-app/src/EventExample.js
+++ React JS Programs - Intermediate/React JS Programs/react-event-app/src/EventExample.js
@@ -1,9 +1,26 @@
 function EventExample() {
 
-
+let handleButton=(event)=> {
+    console.log("Event fired first button");
+    console.log(event)
+}
+let handleTextField=(event)=>{
+    //console.log(event)
+    console.log(event.target.name+" "+event.target.value)
+}
     return(
         <div>
             <h3>Event Example</h3>
+        <input type="button" value="Click Here1" onClick={handleButton}/>
+<input type="button" value="Click Here2" onClick={(e)=> {
+    console.log("Event fired second button");
+    console.log(e)
+}}/>
+<hr/>
+<form>
+    <label>Name</label>
+    <input type="text" name="ename" onChange={handleTextField}/><br/>
+</form>
         </div>
     )
 }
