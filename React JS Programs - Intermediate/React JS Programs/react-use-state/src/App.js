0558d398a5add8ea258cbbb77000b6d402142333
akash <akash300383@gmail.com>
2024-06-09 14:36:04 +0530

    done

--- React JS Programs - Intermediate/React JS Programs/react-use-state/src/App.js
+++ React JS Programs - Intermediate/React JS Programs/react-use-state/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import CouterExample from './CounterExample';
 import UseStateBasicExample from './UseStateBasicExample';
 import ArrayStateVariable from './ArrayStateVariable';
+import ObjectStateVariable from './ObjectStateVariable';
 function App() {
   return (
     <div className="App">
       <h3>React useState Examples</h3>
       {/* <CouterExample></CouterExample> */}
       {/* <UseStateBasicExample></UseStateBasicExample> */}
-      <ArrayStateVariable></ArrayStateVariable>
+      {/* <ArrayStateVariable></ArrayStateVariable> */}
+      <ObjectStateVariable></ObjectStateVariable>
     </div>
   );
 }
---
+++ React JS Programs - Intermediate/React JS Programs/react-use-state/src/ObjectStateVariable.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+function ObjectStateVariable() {
+let [id,setId]=useState(100);
+let [name,setName]=useState("Ravi");
+let [salary,setSalary]=useState(12000);
+    let [employee,setEmployee]=useState({id:102,name:"Vijay",salary:34000}); // object state variable 
+let handleEmployeeInfo1=(event)=> {
+    console.log(id+" "+name+" "+salary)
+    setId(101)
+    setName("Raju");
+    setSalary(14000)
+let emp = {"id":id,"name":name,"salary":salary}
+    console.log(emp);       // we can pass to backend technologies. 
+}
+let handleEmployeeInfo2=(event)=> {
+    //setEmployee(104)          error 
+    //setEmployee({name:"Ajay"});  // updating one property remaining property lose
+   //setEmployee({id:103,name:"Ajay",salary:38000});  // update all property
+   setEmployee({...employee,name:"Ajay"}); // partial object ...previousobject,property:value
+    console.log(employee)
+}
+return(
+    <>
+    <h4>Object State variable</h4>
+    <h5>Employee details</h5>
+    <p>Id is {id} Name is {name} salary is {salary}</p>
+    <input type="button" value="Change Value" onClick={handleEmployeeInfo1}/><br/>
+    <hr/>
+    <h5>Employee details</h5>
+    <p>Id is {employee.id} name is {employee.name} salary is {employee.salary}</p>
+    <input type="button" value="Change Value" onClick={handleEmployeeInfo2}/><br/>
+    </>
+)
+}
+
+export default ObjectStateVariable;
