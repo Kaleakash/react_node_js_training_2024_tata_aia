ab736843391074e73749ef90c5b31a8d07f79806
akash <akash300383@gmail.com>
2024-06-09 17:24:25 +0530

    done

--- React JS Programs - Intermediate/React JS Programs/react-fetch-api/src/FetchApi.js
+++ React JS Programs - Intermediate/React JS Programs/react-fetch-api/src/FetchApi.js
@@ -1,21 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function FetchApi() {
 //let [products,setProducts]=useState(null); // array object state variable
 let [products,setProducts]=useState([{}]); // array object state variable 
+let [loading,isLoading]=useState(true);
 
-let loadData= ()=> {
-    fetch("https://fakestoreapi.com/products").then(response=>response.json()).
-    then(result=>{
-        console.log(result);
-        setProducts(result)
-    }).catch(error=>console.log(error));
-    //console.log(products.length)
-}
+// let loadData= ()=> {
+//     fetch("https://fakestoreapi.com/products").then(response=>response.json()).
+//     then(result=>{
+//         console.log(result);
+//         setProducts(result)
+//     }).catch(error=>console.log(error));
+//     //console.log(products.length)
+// }
+
+useEffect(()=> {
+    
+    setTimeout(()=>{
+
+        fetch("https://fakestoreapi.com/products").then(response=>response.json()).
+        then(result=>{
+            console.log(result);
+            setProducts(result)
+            isLoading(false);
+        }).catch(error=>console.log(error));
+    
+    },5000)
+
+    // fetch("https://fakestoreapi.com/products").then(response=>response.json()).
+    // then(result=>{
+    //     console.log(result);
+    //     setProducts(result)
+    //     isLoading(false);
+    // }).catch(error=>console.log(error));
+
+},[])
+
+
+
+    if(loading){
+        return <h2>Data Loading......</h2>
+    }
+
     return(
         <>
         <h2>Rest API using Fetch function</h2>
-        <input type="button" value="Load Data" onClick={loadData}/><br/>
+        {/* <input type="button" value="Load Data" onClick={loadData}/><br/> */}
         {products.map((product,index)=>
             
             <div key={index}>
@@ -24,7 +54,6 @@ let loadData= ()=> {
             </div>)
             
             }
-       
         </>
     )
 }
