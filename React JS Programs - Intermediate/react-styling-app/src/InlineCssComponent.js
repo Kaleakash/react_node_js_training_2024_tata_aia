ede1474215307fb5ceca2f143007c454ab92fd38
akash <akash300383@gmail.com>
2024-06-15 12:02:08 +0530

    done

--- React JS Programs - Intermediate/react-styling-app/src/InlineCssComponent.js
+++ React JS Programs - Intermediate/react-styling-app/src/InlineCssComponent.js
@@ -1,20 +1,18 @@
 import imagename from './akash.jpg';
 import * as obj from './name.js'
 
-//import {fname,designation,marks} from './name.js'
-//import {fname,designation,marks} from './name1.js'
-
-//import { useEffect,use } from 'react';
-//import {designation} from './name.js'
 function InlineCssComponent() {
 
+const styleInfo = {border:"yellow 1px solid",color:"red"}
 
 return(
-    <section>
+<section style={{border:"red 1px solid",width:"50%",backgroundColor:'pink',marginLeft:"300px",borderRadius:"10%"}}>
         <img src="akash.jpg" width="200px" height="200px"/>
         <img src={imagename} width="200px" height="200px"></img>
-        <p>This is one of trainer and developer blogs {obj.marks}</p>
-        <h3> <span>{obj.fname} - {obj.designation}</span> Full Stack Trainer</h3>
+        <img src={obj.iname} width="200px" height="200px"/>
+<p style={styleInfo}>This is one of trainer and developer blogs {obj.marks.map(n=><li>{n}</li>)}</p>
+<h3 style={styleInfo}> <span>{obj.fname} - {obj.designation}</span> Full Stack Trainer</h3>
+
     </section>
 )
 }
--- React JS Programs - Intermediate/react-styling-app/src/name.js
+++ React JS Programs - Intermediate/react-styling-app/src/name.js
@@ -1,7 +1,10 @@
+import imagename from './akash.jpg';
+
 export let fname = "akash kale";
 
 export let designation = "trainer";
 export let marks = [10,20,30,40];
+export let iname = imagename;
 
 //export {fname,designation,marks};
 
