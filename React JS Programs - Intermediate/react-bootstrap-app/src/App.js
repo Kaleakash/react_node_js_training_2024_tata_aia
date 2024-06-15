6f6f0b6d3463e35ee823bca922f4f6b072da9ce4
akash <akash300383@gmail.com>
2024-06-15 14:47:07 +0530

    done

---
+++ React JS Programs - Intermediate/react-bootstrap-app/src/App.js
@@ -0,0 +1,13 @@
+import BasicComponent from './BasicComponent';
+import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div>
+     <BasicComponent></BasicComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ React JS Programs - Intermediate/react-bootstrap-app/src/BasicComponent.js
@@ -0,0 +1,20 @@
+function BasicComponent() {
+
+return(
+    <div>
+    <h2>Welcome to Boostrap with React JS</h2>
+<input type="button" value="Click Here" style={{backgroundColor:"yellowgreen",color:'whitesmoke'}}/>
+<input type="button" value="Click Here" className="btn"/>
+<input type="button" value="Click Here" className="btn btn-success"/>
+<input type="button" value="Click Here" className="btn btn-primary"/>
+<input type="button" value="Click Here" className="btn btn-secondary"/>
+<input type="button" value="Click Here" className="btn btn-danger"/>
+<input type="button" value="Click Here" className="btn btn-info"/>
+<input type="button" value="Click Here" className="btn btn-warning"/>
+<input type="button" value="Click Here" className="btn btn-warning" style={{color:'red'}}/>
+<input type="button" value="Click Here" className="btn btn-link"/>
+    </div>
+    )
+}
+
+export default BasicComponent;
