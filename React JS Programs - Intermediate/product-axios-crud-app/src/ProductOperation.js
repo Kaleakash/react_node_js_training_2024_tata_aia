42d1ebe8e5e31c15c5a10c25e74687bc6d918239
akash <akash300383@gmail.com>
2024-06-16 15:27:28 +0530

    done

--- React JS Programs - Intermediate/product-axios-crud-app/src/ProductOperation.js
+++ React JS Programs - Intermediate/product-axios-crud-app/src/ProductOperation.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function ProductOperation() {
+let [products,setProducts]=useState([{}]);  // to store product array 
 
 useEffect(()=> {
 
 let loadProductInfo= ()=>{
-axios.get("http://localhost:3000/products").then(result=>console.log(result.data)).catch(error=>console.log(error))
+axios.get("http://localhost:3000/products").then(result=>setProducts(result.data)).catch(error=>console.log("catch block"+error))
 }
 
 loadProductInfo();
@@ -17,8 +18,29 @@ loadProductInfo();
     return(
         <div>
             <h3>Product Management System using Axios</h3>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>PId</th>
+                        <th>PName</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product,index)=>
+                    <tr key={index}>
+                        <td>{product.id}</td>
+                        <td>{product.pname}</td>
+                        <td>{product.price}</td>
+                    </tr>
+                    )}
+                </tbody>
+            </table>
         </div>
     )
 }
 
 export default ProductOperation;
+
+
