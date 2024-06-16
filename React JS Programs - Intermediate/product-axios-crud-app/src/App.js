9c8c4817c038bd5cea6807ed4e8a787d9dbb4138
akash <akash300383@gmail.com>
2024-06-16 15:01:56 +0530

    done

---
+++ React JS Programs - Intermediate/product-axios-crud-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductOperation from './ProductOperation';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductOperation></ProductOperation>
+    </div>
+  );
+}
+
+export default App;
---
+++ React JS Programs - Intermediate/product-axios-crud-app/src/ProductOperation.js
@@ -0,0 +1,10 @@
+function ProductOperation() {
+
+    return(
+        <div>
+            <h3>Product Management System using Axios</h3>
+        </div>
+    )
+}
+
+export default ProductOperation;
