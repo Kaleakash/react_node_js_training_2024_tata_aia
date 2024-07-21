1c772a1612c9baa7a397a9e3d9210773c96e852d
akash <akash300383@gmail.com>
2024-07-21 10:38:57 +0530

    done

---
+++ React Adv/Programs/react-with-redux-employee-app/src/reducer.js
@@ -0,0 +1,17 @@
+let initialState = {
+    msg:"Welcome to React with Redux",
+
+    employees:[
+        {id:100,name:"Ravi",age:21},
+        {id:101,name:"Ajay",age:24},
+    ]
+}
+
+
+function reducer(state=initialState,action){
+
+
+    return state;
+}
+
+export default reducer;
