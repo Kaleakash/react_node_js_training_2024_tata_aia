77bc0cbdd6a01ffc0f7608804530c73a34a7ca56
akash <akash300383@gmail.com>
2024-08-10 16:51:23 +0530

    done

--- React Adv/Programs/react-with-formik-yup-app/src/BasicFormikForm.js
+++ React Adv/Programs/react-with-formik-yup-app/src/BasicFormikForm.js
@@ -2,7 +2,17 @@ import { useFormik } from "formik";
 
 function BasicFormikForm() {
 
-let {handleSubmit,handleChange,errors}    = useFormik({            
+let validate = (values)=> {
+    const errors={}
+    if(!values.firstname){
+        errors.firstname="First Name required"
+    }
+    if(!values.lastname){
+        errors.lastname="Last Name required"
+    }
+    return errors;
+}
+let {handleSubmit,handleChange,errors,values,handleReset}    = useFormik({            
             initialValues:{
                 firstname:"",
                 lastname:""
@@ -10,31 +20,26 @@ let {handleSubmit,handleChange,errors}    = useFormik({
             onSubmit:(values)=> {
                 console.log("event fired")
                 console.log(values)
+                // logic to store the data  
             },
-            validate:(values)=> {
-                if(values.firstname.length==0){
-                    errors.firstname="First Name is required"  
-                }else if(values.lastname.length==0){
-                    errors.firstname=""
-                    errors.lastname="Last Name is required"
-                }else {
-                    errors.firstname=""
-                    errors.lastname=""
-                    return true
-                }    
-            }
+            onReset:(values)=> {
+                console.log("reset called.")
+                values.firstname=""
+                values.lastname=""
+            },
+            validate
         })
     return(
         <div>  
             <h2>Simple Basic Form</h2>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} onReset={handleReset}>
             <label>First Name</label>
-            <input type="text" name="firstname" onChange={handleChange}/>
-                <span style={{"color":"red"}}>{errors.firstname}</span>
+            <input type="text" name="firstname" onChange={handleChange}  value={values.firstname}/>
+                <span style={{"color":"red"}}>{errors.firstname?errors.firstname:null}</span>
             <br/>
             <label>Last Name</label>
-            <input type="text" name="lastname" onChange={handleChange}/>
-            <span style={{"color":"red"}}>{errors.lastname}</span>
+            <input type="text" name="lastname" onChange={handleChange} value={values.lastname}/>
+            <span style={{"color":"red"}}>{errors.lastname?errors.lastname:null}</span>
             <br/>
             <input type="submit" value="submit"/>
             <input type="reset" value="reset"/>
