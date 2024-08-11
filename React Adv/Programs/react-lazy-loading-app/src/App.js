c9aed39ddb8851b4387894ccab866f704145843b
akash <akash300383@gmail.com>
2024-08-11 12:25:18 +0530

    done

--- React Adv/Programs/react-lazy-loading-app/src/App.js
+++ React Adv/Programs/react-lazy-loading-app/src/App.js
@@ -2,12 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import ImageComponent from './ImageComponent';
 import ImageLazyLoadComponent from './ImageLazyLoadComponent';
+//import LazyComponent from './LazyComponent';
+import { lazy, Suspense } from 'react';
+
+const MyLazyComponent = lazy(()=>import("./LazyComponent"))
 
 function App() {
   return (
     <div className="App">
+      <h2>Main Component</h2>
+      
       {/* <ImageComponent></ImageComponent> */}
-      <ImageLazyLoadComponent></ImageLazyLoadComponent>
+      {/* <ImageLazyLoadComponent></ImageLazyLoadComponent> */}
+      {/* <LazyComponent></LazyComponent> */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <MyLazyComponent/>
+      </Suspense>
     </div>
   );
 }
---
+++ React Adv/Programs/react-lazy-loading-app/src/LazyComponent.js
@@ -0,0 +1,12 @@
+function LazyComponent () {
+
+    return(
+        <div>
+            <h2>I am a Lazy Component</h2>
+        </div>
+    )
+
+}
+
+
+export default LazyComponent;
