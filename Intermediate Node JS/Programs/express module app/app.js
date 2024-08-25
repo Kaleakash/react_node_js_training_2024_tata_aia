2a69e840704696a9eca31cf1cd386db0554c1708
akash <akash300383@gmail.com>
2024-08-25 15:13:55 +0530

    done

--- Intermediate Node JS/Programs/express module app/app.js
+++ Intermediate Node JS/Programs/express module app/app.js
@@ -1,6 +1,10 @@
 let express = require("express");  // load the modules 
 let app = express();            // get top most function reference of express module 
+let bodyParser = require("body-parser")
 
+//middleware 
+
+app.use(bodyParser.urlencoded({extended:true}));  // enable post raw data to extract in app
 
 // http://localhsot:9090/
 
@@ -39,14 +43,26 @@ app.get("/checkLoginDetails",(req,res)=> {
     let emailid = req.query["emailid"]
     let password = req.query["password"]
     if(emailid=="raj@gmail.com" && password=="123"){
-            res.send("Successfully login")
+            res.send("Successfully login with get method")
     }else {
-            res.send("Failure try once again")
+            res.send("Failure try once again with get method")
     }
 })
 
 app.get("/**",(req,res)=> {
     res.send("Page not found")
+})
+
+
+// post method 
+app.post("/checkLoginDetails",(req,res)=> {
+    let data = req.body;
+    console.log(data)
+    if(data.emailid=="raj@gmail.com" && data.password=="123"){
+        res.send("Successfully login with post method")
+}else {
+        res.send("Failure try once again with post method")
+}
 })
 
 app.listen(9090,()=> {
