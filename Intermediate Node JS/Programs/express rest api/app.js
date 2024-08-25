3110fad0d2ecd655770277c01afcf1e9bd940ece
akash <akash300383@gmail.com>
2024-08-25 17:45:31 +0530

    done

---
+++ Intermediate Node JS/Programs/express rest api/app.js
@@ -0,0 +1,10 @@
+let express = require("express");
+let app = express();
+let product = {id:100,name:"Computer",price:45000}
+
+app.get("/product",(req,res)=> {
+    res.json(product)
+})
+
+
+app.listen(9090,()=>console.log("express server up"))
