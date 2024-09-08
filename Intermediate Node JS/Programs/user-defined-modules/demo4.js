5db9e9ba92b269580565b25a2b682d55572ba4f6
Akash <akash300383@gmail.com>
2024-09-08 16:10:02 +0530

    done

---
+++ Intermediate Node JS/Programs/user-defined-modules/demo4.js
@@ -0,0 +1,8 @@
+class Employee {
+    disEmpInfo() {
+        console.log("Display Employee information")
+    }
+}
+export let fname = "Ajay Kumar";
+//module.exports={Employee}
+export default Employee;
--- Intermediate Node JS/Programs/user-defined-modules/main.js
+++ Intermediate Node JS/Programs/user-defined-modules/main.js
@@ -1,10 +1,15 @@
-let obj1 = require("./demo1")       // we load the modules 
-let obj2 = require("./demo2");
-let obj3 = require("./demo3")
-obj1.display1();
-obj2.display2();
-obj1.display();
-obj2.display();
-console.log(obj3.fname);
-console.log(obj3.sayHello("raj"))
+// let obj1 = require("./demo1")       // we load the modules 
+// let obj2 = require("./demo2");
+// let obj3 = require("./demo3")
+// obj1.display1();
+// obj2.display2();
+// obj1.display();
+// obj2.display();
+// console.log(obj3.fname);
+// console.log(obj3.sayHello("raj"))
 //display1();
+
+import Employee,{fname} from "./demo4.js";
+let emp1 = new Employee();
+emp1.disEmpInfo();
+console.log(fname)
