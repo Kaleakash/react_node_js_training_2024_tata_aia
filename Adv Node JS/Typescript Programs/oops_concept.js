638013c2eadcd22c5e5b9ca9623f35425c0336d2
Akash <akash300383@gmail.com>
2024-09-21 14:46:24 +0530

    done

---
+++ Adv Node JS/Typescript Programs/oops_concept.js
@@ -0,0 +1,9 @@
+// // creating object in literal style 
+// let emp1={id:100,name:"Ravi",age:21};
+// console.log(emp1.id+" "+emp1.name+" "+emp1.age)
+// let emp2 = {id:101,name:"Ramesh",salary:34000}
+// let emp3 = {id:"afas123",fname:"Raju",lname:"Patil"}
+let emp1 = { id: 100, name: "Ravi", salary: 45000, age: 24 };
+let emp2 = { id: 101, name: "Raj", salary: 46000 };
+console.log("id " + emp1.id + " name " + emp1.name + " salary " + emp1.salary + " Age is " + emp1.age);
+console.log("id " + emp2.id + " name " + emp2.name + " salary " + emp2.salary + " Age is " + emp2.age);
---
+++ Adv Node JS/Typescript Programs/oops_concept.ts
@@ -0,0 +1,16 @@
+// // creating object in literal style 
+// let emp1={id:100,name:"Ravi",age:21};
+// console.log(emp1.id+" "+emp1.name+" "+emp1.age)
+// let emp2 = {id:101,name:"Ramesh",salary:34000}
+// let emp3 = {id:"afas123",fname:"Raju",lname:"Patil"}
+
+interface EmployeeType {
+    id:number;
+    name:string;
+    salary:number;
+    age?:number;
+}
+let emp1:EmployeeType={id:100,name:"Ravi",salary:45000,age:24}
+let emp2:EmployeeType={id:101,name:"Raj",salary:46000}
+console.log("id "+emp1.id+" name "+emp1.name+" salary "+emp1.salary+" Age is "+emp1.age)
+console.log("id "+emp2.id+" name "+emp2.name+" salary "+emp2.salary+" Age is "+emp2.age)
