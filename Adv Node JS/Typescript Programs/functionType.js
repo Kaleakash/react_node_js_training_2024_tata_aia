696b32184ed886fff9f4f4ae7e2962c5990ce93e
Akash <akash300383@gmail.com>
2024-09-21 12:51:34 +0530

    done

--- Adv Node JS/Typescript Programs/functionType.js
+++ Adv Node JS/Typescript Programs/functionType.js
@@ -33,60 +33,75 @@
 //     //return "success"
 //     return 0;
 // }
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 // empDetails function with default value for parameter 
-function empInfo(id, fname, age, result) {
-    if (fname === void 0) { fname = "UnKnown"; }
-    if (age === void 0) { age = 18; }
-    if (result === void 0) { result = false; }
-    console.log("id " + id);
-    console.log("name " + fname);
-    console.log("age " + age);
-    console.log("result " + result);
+// function empInfo(id:number,fname:string="UnKnown",age:number=18,result:boolean=false):void {
+//     console.log("id "+id)
+//     console.log("name "+fname)
+//     console.log("age "+age)
+//     console.log("result "+result);
+// }
+// empInfo(100,"Ravi",21,true);
+// empInfo(101,"Ramesh",34)
+// empInfo(102,"Ajay")
+// empInfo(103)
+// // empDetails function with optional value for parameter 
+// function empInfo1(id:number,fname?:string,age?:number,result?:boolean):void {
+//     console.log("id "+id)
+//     console.log("name "+fname)
+//     console.log("age "+age)
+//     console.log("result "+result);
+// }
+// empInfo1(100,"Ravi",21,true);
+// empInfo1(101,"Ramesh",34)
+// empInfo1(102,"Ajay")
+// empInfo1(103)
+// // with array parameter we need to pass empty array or array with value. 
+// function studentInfo(sid:number,sname:string,age:number,skillset:Array<string>):void {
+//     console.log("sid "+sid+"sname "+sname+" age "+age+"Number of skillset "+skillset.length)
+// }
+// let skillSetInfo:string[]=["C","C++","C#"]
+// studentInfo(1,"Ravi",21,["Java"]);
+// studentInfo(2,"Raj",22,["NodeJS","Angular","ReactJS","Python"]);
+// studentInfo(3,"Rajesh",32,[]);
+// studentInfo(4,"Ajay",34,skillSetInfo);  //skillSet array variable. 
+// // with array parameter we need to pass empty array or array with value. 
+// // ...variableName it receive 0 or 1 or many parameter like Java main(String...args)
+// function studentInfo1(sid:number,sname:string,age:number,...skillset:Array<string>):void {  //...skillset rest 
+//     console.log("sid "+sid+"sname "+sname+" age "+age+"Number of skillset "+skillset.length)
+// }
+// studentInfo1(1,"Ravi",21,"Java");
+// studentInfo1(2,"Raj",22,"NodeJS","Angular","ReactJS","Python");
+// studentInfo1(3,"Rajesh",32);
+// studentInfo1(4,"Ajay",34,skillSetInfo[0],skillSetInfo[1],skillSetInfo[2]);
+// studentInfo1(4,"Ajay",34,...skillSetInfo);      // using spread operator we are passing value to rest operator 
+// normal style function 
+display1();
+function display1() {
+    console.log("normal function");
 }
-empInfo(100, "Ravi", 21, true);
-empInfo(101, "Ramesh", 34);
-empInfo(102, "Ajay");
-empInfo(103);
-// empDetails function with optional value for parameter 
-function empInfo1(id, fname, age, result) {
-    console.log("id " + id);
-    console.log("name " + fname);
-    console.log("age " + age);
-    console.log("result " + result);
-}
-empInfo1(100, "Ravi", 21, true);
-empInfo1(101, "Ramesh", 34);
-empInfo1(102, "Ajay");
-empInfo1(103);
-// with array parameter we need to pass empty array or array with value. 
-function studentInfo(sid, sname, age, skillset) {
-    console.log("sid " + sid + "sname " + sname + " age " + age + "Number of skillset " + skillset.length);
-}
-var skillSetInfo = ["C", "C++", "C#"];
-studentInfo(1, "Ravi", 21, ["Java"]);
-studentInfo(2, "Raj", 22, ["NodeJS", "Angular", "ReactJS", "Python"]);
-studentInfo(3, "Rajesh", 32, []);
-studentInfo(4, "Ajay", 34, skillSetInfo); //skillSet array variable. 
-// with array parameter we need to pass empty array or array with value. 
-// ...variableName it receive 0 or 1 or many parameter like Java main(String...args)
-function studentInfo1(sid, sname, age) {
-    var skillset = [];
-    for (var _i = 3; _i < arguments.length; _i++) {
-        skillset[_i - 3] = arguments[_i];
-    }
-    console.log("sid " + sid + "sname " + sname + " age " + age + "Number of skillset " + skillset.length);
-}
-studentInfo1(1, "Ravi", 21, "Java");
-studentInfo1(2, "Raj", 22, "NodeJS", "Angular", "ReactJS", "Python");
-studentInfo1(3, "Rajesh", 32);
-studentInfo1(4, "Ajay", 34, skillSetInfo[0], skillSetInfo[1], skillSetInfo[2]);
-studentInfo1.apply(void 0, __spreadArray([4, "Ajay", 34], skillSetInfo, false)); // using spread operator we are passing value to rest operator 
+display1();
+// expression style function 
+var display3 = function display2() {
+    console.log("normal function");
+};
+display3();
+// expression style function 
+//display4();
+var display4 = function () {
+    console.log("expression style function");
+};
+display4();
+// arrow function 
+var display5 = function () { return console.log("arrow style function"); };
+display4();
+// addition of two number using expression style 
+var addNumber1 = function (a, b) {
+    var sum = a + b;
+    return sum;
+};
+console.log("sum of two number is using expression style" + addNumber1(100, 200));
+// addition of two number using arrow style 
+var addNumber2 = function (a, b) { return a + b; };
+console.log("sum of two number is using arrow style" + addNumber2(200, 300));
+var addNumber3 = function (a, b) { return a + b; };
+console.log("sum of two number is using arrow style" + addNumber3(200, 300));
--- Adv Node JS/Typescript Programs/functionType.ts
+++ Adv Node JS/Typescript Programs/functionType.ts
@@ -35,44 +35,78 @@
 // }
 
 // empDetails function with default value for parameter 
-function empInfo(id:number,fname:string="UnKnown",age:number=18,result:boolean=false):void {
-    console.log("id "+id)
-    console.log("name "+fname)
-    console.log("age "+age)
-    console.log("result "+result);
-}
-empInfo(100,"Ravi",21,true);
-empInfo(101,"Ramesh",34)
-empInfo(102,"Ajay")
-empInfo(103)
-// empDetails function with optional value for parameter 
-function empInfo1(id:number,fname?:string,age?:number,result?:boolean):void {
-    console.log("id "+id)
-    console.log("name "+fname)
-    console.log("age "+age)
-    console.log("result "+result);
-}
-empInfo1(100,"Ravi",21,true);
-empInfo1(101,"Ramesh",34)
-empInfo1(102,"Ajay")
-empInfo1(103)
-// with array parameter we need to pass empty array or array with value. 
-function studentInfo(sid:number,sname:string,age:number,skillset:Array<string>):void {
-    console.log("sid "+sid+"sname "+sname+" age "+age+"Number of skillset "+skillset.length)
-}
-let skillSetInfo:string[]=["C","C++","C#"]
-studentInfo(1,"Ravi",21,["Java"]);
-studentInfo(2,"Raj",22,["NodeJS","Angular","ReactJS","Python"]);
-studentInfo(3,"Rajesh",32,[]);
-studentInfo(4,"Ajay",34,skillSetInfo);  //skillSet array variable. 
+// function empInfo(id:number,fname:string="UnKnown",age:number=18,result:boolean=false):void {
+//     console.log("id "+id)
+//     console.log("name "+fname)
+//     console.log("age "+age)
+//     console.log("result "+result);
+// }
+// empInfo(100,"Ravi",21,true);
+// empInfo(101,"Ramesh",34)
+// empInfo(102,"Ajay")
+// empInfo(103)
+// // empDetails function with optional value for parameter 
+// function empInfo1(id:number,fname?:string,age?:number,result?:boolean):void {
+//     console.log("id "+id)
+//     console.log("name "+fname)
+//     console.log("age "+age)
+//     console.log("result "+result);
+// }
+// empInfo1(100,"Ravi",21,true);
+// empInfo1(101,"Ramesh",34)
+// empInfo1(102,"Ajay")
+// empInfo1(103)
+// // with array parameter we need to pass empty array or array with value. 
+// function studentInfo(sid:number,sname:string,age:number,skillset:Array<string>):void {
+//     console.log("sid "+sid+"sname "+sname+" age "+age+"Number of skillset "+skillset.length)
+// }
+// let skillSetInfo:string[]=["C","C++","C#"]
+// studentInfo(1,"Ravi",21,["Java"]);
+// studentInfo(2,"Raj",22,["NodeJS","Angular","ReactJS","Python"]);
+// studentInfo(3,"Rajesh",32,[]);
+// studentInfo(4,"Ajay",34,skillSetInfo);  //skillSet array variable. 
 
-// with array parameter we need to pass empty array or array with value. 
-// ...variableName it receive 0 or 1 or many parameter like Java main(String...args)
-function studentInfo1(sid:number,sname:string,age:number,...skillset:Array<string>):void {  //...skillset rest 
-    console.log("sid "+sid+"sname "+sname+" age "+age+"Number of skillset "+skillset.length)
+// // with array parameter we need to pass empty array or array with value. 
+// // ...variableName it receive 0 or 1 or many parameter like Java main(String...args)
+// function studentInfo1(sid:number,sname:string,age:number,...skillset:Array<string>):void {  //...skillset rest 
+//     console.log("sid "+sid+"sname "+sname+" age "+age+"Number of skillset "+skillset.length)
+// }
+// studentInfo1(1,"Ravi",21,"Java");
+// studentInfo1(2,"Raj",22,"NodeJS","Angular","ReactJS","Python");
+// studentInfo1(3,"Rajesh",32);
+// studentInfo1(4,"Ajay",34,skillSetInfo[0],skillSetInfo[1],skillSetInfo[2]);
+// studentInfo1(4,"Ajay",34,...skillSetInfo);      // using spread operator we are passing value to rest operator 
+
+
+// normal style function 
+display1();
+function display1() {
+    console.log("normal function")
 }
-studentInfo1(1,"Ravi",21,"Java");
-studentInfo1(2,"Raj",22,"NodeJS","Angular","ReactJS","Python");
-studentInfo1(3,"Rajesh",32);
-studentInfo1(4,"Ajay",34,skillSetInfo[0],skillSetInfo[1],skillSetInfo[2]);
-studentInfo1(4,"Ajay",34,...skillSetInfo);      // using spread operator we are passing value to rest operator 
+display1();
+// expression style function 
+let display3 = function display2() {
+                    console.log("normal function")
+                }
+display3();
+// expression style function 
+//display4();
+let display4 = function() {
+        console.log("expression style function")
+}
+display4();
+// arrow function 
+let display5 = ()=>console.log("arrow style function")
+display4();
+// addition of two number using expression style 
+let addNumber1 = function(a:number,b:number):number{
+    let sum:number = a+b;
+    return sum;
+}
+console.log("sum of two number is using expression style"+addNumber1(100,200))
+// addition of two number using arrow style 
+let addNumber2 = (a:number,b:number):number=>a+b
+console.log("sum of two number is using arrow style"+addNumber2(200,300))
+let addNumber3 = (a,b)=>a+b
+console.log("sum of two number is using arrow style"+addNumber3(200,300))
+
