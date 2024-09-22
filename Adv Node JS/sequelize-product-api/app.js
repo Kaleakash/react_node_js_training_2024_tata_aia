22f8a11bdeb802ab6686a8d6a24075aad318bb01
Akash <akash300383@gmail.com>
2024-09-22 15:48:06 +0530

    done

--- Adv Node JS/sequelize-product-api/app.js
+++ Adv Node JS/sequelize-product-api/app.js
@@ -45,6 +45,31 @@ app.post("/store_product",async (req,res)=> {
         res.json({"msg":error.message})
     }
 })
-
+// retrieve all records from product model 
+// http://localhost:9090/find_all    
+app.get("/find_all",async(req,res)=> {
+    try{
+    let products = await ProductModel.findAll();            // like select query 
+    res.json(products);
+    }catch(error){
+        res.json({"msg":error.message});
+    }
+})
+// retrieve product details using id as pk 
+// http://localhost:9090/find_by_id/1
+app.get("/find_by_id/:id",async (req,res)=> {
+    try{
+    let id = req.params.id;    
+    let product = await ProductModel.findByPk(id);
+    console.log(product);
+    if(product==null){
+        res.json({"msg":"product not present"})
+    }else {
+        res.json(product);
+    }
+    }catch(error){
+        res.json({"msg":error.message});
+    }
+})
 
 app.listen(9090,()=>console.log("Server running on port number 9090"));
