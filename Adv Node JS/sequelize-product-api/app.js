1ae59e6ecb4d033f8f22bf342a5506b1a2d971a5
Akash <akash300383@gmail.com>
2024-09-22 15:26:37 +0530

    done

--- Adv Node JS/sequelize-product-api/app.js
+++ Adv Node JS/sequelize-product-api/app.js
@@ -1,13 +1,10 @@
 let express = require("express");
 let sequelizeRef = require("sequelize");
-
 let app = express();
-
 // middleware 
 app.use(express.json());            // enable json data from request body 
-
 // database information 
-                                    // 1st database, 2nd username, 3rd password 
+// 1st database, 2nd username, 3rd password  
 let s_ref = new sequelizeRef.Sequelize("my_db","postgres","postgres",{
     host:"localhost",
     port:5432,
@@ -15,7 +12,7 @@ let s_ref = new sequelizeRef.Sequelize("my_db","postgres","postgres",{
 })
 
 // define model 
-let Product = s_ref.define("Product",{
+let ProductModel = s_ref.define("Product",{
     pname: {
         type:sequelizeRef.DataTypes.STRING,
         allowNull:false
@@ -34,6 +31,20 @@ s_ref.sync().then(result=> {
 }).catch(error=> {
     console.log(error)
 });
+
+// Rest API 
+// http://localhost:9090/store_product      
+app.post("/store_product",async (req,res)=> {
+    let product = req.body;
+    //console.log(product);
+    try{
+    let result =  await ProductModel.create(product);           // asynchronous we are not writing insert query 
+    //console.log(result)
+    res.json({"msg":result}); 
+    }catch(error){
+        res.json({"msg":error.message})
+    }
+})
 
 
 app.listen(9090,()=>console.log("Server running on port number 9090"));
