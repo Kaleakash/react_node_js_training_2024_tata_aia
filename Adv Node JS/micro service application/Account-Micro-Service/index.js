53ba2f0df479d12505f26a8f375ee321072e97f3
Akash <akash300383@gmail.com>
2024-10-05 15:28:06 +0530

    done

--- Adv Node JS/micro service application/Account-Micro-Service/index.js
+++ Adv Node JS/micro service application/Account-Micro-Service/index.js
@@ -1,9 +1,15 @@
 const express = require("express")
-const Consul = require("consul");      // load the module 
+    const Consul = require("consul");      // load the module 
 const app = express();
-const consul=new Consul();          // created reference of Consul
-let port = 9191;
+    const consul=new Consul();          // created reference of Consul
+let port = 9494;
 // it is use to register on consul server or service 
+let accounts =[
+    {"accno":100,name:"Ravi","emailid":"ravi@gmail.com","amount":1500},
+    {"accno":101,name:"Ramesh","emailid":"ramesh@gmail.com","amount":1800},
+    {"accno":102,name:"Rajesh","emailid":"rajesh@gmail.com","amount":2500},
+    {"accno":103,name:"Ram","emailid":"ram@gmail.com","amount":2800}
+]
 consul.agent.service.register({
     name:"account-service",
     address:"localhost",
@@ -18,7 +24,25 @@ consul.agent.service.register({
         console.log("Register")
     }
 })
+// http://localhost:9191
 app.get("/",(req,res)=> {
     res.send("Welcome to Account Micro Service")
 })
+// http://localhost:9191/accounts
+app.get("/accounts",(req,res)=> {
+    res.json(accounts);
+})
+
+// http://localhost:9191/find_balance/ravi@gmail.com
+// http://localhost:9191/find_balance/ajay@gmail.com
+app.get("/find_balance/:emailid",(req,res)=> {
+    let emailid = req.params.emailid;
+    let result = accounts.find(a=>a.emailid==emailid);
+    if(result==undefined){
+        res.json({"msg":"Account not present"})
+    }else {
+        res.json({"msg":`Your balance amount is ${result.amount}`});
+    }
+})
+
 app.listen(port,()=> console.log(`Micro service up on port number ${port}`))
--- Adv Node JS/micro service application/Gpay-Micro-Service/index.js
+++ Adv Node JS/micro service application/Gpay-Micro-Service/index.js
@@ -1,8 +1,18 @@
 const express = require("express")
 const Consul = require("consul");      // load the module 
+const { default: axios } = require("axios");
 const app = express();
 const consul=new Consul();          // created reference of Consul
 let port = 9292;
+
+let gpay_accounts =[
+    {"gpayid":1,"emailid":"ravi@gmail.com"},
+    {"gpayid":2,"emailid":"ramesh@gmail.com"},
+    {"gpayid":3,"emailid":"rajesh@gmail.com"},
+    {"gpayid":4,"emailid":"ram@gmail.com"},
+    {"gpayid":5,"emailid":"ajay@gmail.com"}
+]
+
 // it is use to register on consul server or service 
 consul.agent.service.register({
     name:"google-pay-service",
@@ -18,7 +28,43 @@ consul.agent.service.register({
         console.log("Register")
     }
 })
+// http://localhost:9292
 app.get("/",(req,res)=> {
     res.send("Welcome to Googl Pay Micro Service")
 })
+// http://localhost:9292/service_test 
+
+app.get("/service_test",async (req,res)=> {
+    try{
+    let service = await consul.agent.service.list();
+    res.json({"msg":service})
+    }catch(error){
+        res.json({"msg":error.message})
+    }
+})
+// http://localhost:9292/find_balance/1
+// http://localhost:9292/find_balance/10
+app.get("/find_balance/:g_pay_id",async (req,res)=> {
+    try{
+    let g_pay_id = req.params.g_pay_id
+    let result = gpay_accounts.find(g=>g.gpayid==g_pay_id);
+    if(result==undefined){
+            res.json({"msg":"G Pay Account not exists"})
+    } else {
+        let service = await consul.agent.service.list();
+        let account_service = Object.values(service).find(s=>s.Service=="account-service") 
+        //console.log(service);   
+        //console.log(account_service);
+        const serviceUrl = `http://${account_service.Address}:${account_service.Port}/find_balance/${result.emailid}`;
+        let response = await axios.get(serviceUrl);     // it is calling account micro service 
+        console.log(response.data.msg);     
+        res.json({"msg":response.data.msg}) 
+    } 
+    //let service = await consul.agent.service.list();
+    //res.json({"msg":service})
+    }catch(error){
+        res.json({"msg":error.message})
+    }
+})
+
 app.listen(port,()=> console.log(`Micro service up on port number ${port}`))
